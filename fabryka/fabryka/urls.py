727427b1c99bbd6bc27df20f21f0c9b28853ad08
Damian <Rutkowskid98@gmail.com>
2020-06-02 18:00:00 +0200

    Main page working

--- fabryka/fabryka/urls.py
+++ fabryka/fabryka/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('factory/', include('factory.urls')),
+    path('', include('factory.urls')),
     path('admin/', admin.site.urls),
 ]
