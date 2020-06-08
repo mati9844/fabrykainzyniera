0aa1e8cb41a810d23d1712eb459a7f92a081d083
Damian <Rutkowskid98@gmail.com>
2020-06-08 22:16:56 +0200

    added login future

--- fabryka/factory/urls.py
+++ fabryka/factory/urls.py
@@ -3,8 +3,14 @@ from django.urls import path
 
 from . import views
 
+#
+from django.views.generic.base import TemplateView
+#
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('login/', views.logowanie, name='Logowanie'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('user/', TemplateView.as_view(template_name='Strona-2.html'), name='home'),
 
 ]
--- fabryka/factory/views.py
+++ fabryka/factory/views.py
@@ -8,6 +8,12 @@ def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+
 def logowanie(request):
     template = loader.get_template('Logowanie.html')
+    return HttpResponse(template.render())
+
+
+def user_page(request):
+    template = loader.get_template('Strona-2.html')
     return HttpResponse(template.render())
